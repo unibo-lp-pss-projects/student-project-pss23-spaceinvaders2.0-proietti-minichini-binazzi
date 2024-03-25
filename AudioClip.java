192c41815aeab5b9ea96b3370bf6de8a08960fb9
Angy1304 <146926904+Angy1304@users.noreply.github.com>
2024-03-25 12:59:07 +0100

    Create AudioClip.java

---
+++ AudioClip.java
@@ -0,0 +1,29 @@
+package org.example
+
+import javax.sound.sampled.Clip;
+
+public class AudioClip {
+    public Clip clip;
+
+    public AudioClip(Clip clip) {
+        this.clip = clip;
+    }
+
+    public Clip getClip() {
+        return clip;
+    }
+
+    public void play() {
+        if (clip != null) {
+            clip.stop();
+            clip.setFramePosition(0);
+            clip.start();
+        }
+    }
+
+    public void loop() {
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+}
